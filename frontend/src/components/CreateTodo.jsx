378f4763173f057aef93b61e5404cbabca6c98c1
raashed <mohammedraashed10@gmail.com>
2024-12-16 19:07:00 +0530

    rendering the todos from components -> Todos.jsx

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,7 +1,13 @@
 export function CreateTodo(){
     return <div>
-        <input type="text" placeholder="title"/> <br></br>
-        <input type="text" placeholder="description"/> <br></br>
+        <input style = {{
+            padding : 10,
+            margin : 10
+        }} type="text" placeholder="title"/> <br></br>
+        <input style = {{
+            padding : 10,
+            margin : 10
+        }} type="text" placeholder="description"/> <br></br>
 
         <button> Add a todo</button>
     </div>
