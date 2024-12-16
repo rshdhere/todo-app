292874a0492793ed1caa6fb39f5e8abaa1274f77
raashed <mohammedraashed10@gmail.com>
2024-12-16 18:46:16 +0530

    added frontend and src -> components -> CreateTodo.jsx

---
+++ frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,8 @@
+export function CreateTodo(){
+    return <div>
+        <input type="text" placeholder="title"/> <br></br>
+        <input type="text" placeholder="description"/> <br></br>
+
+        <button> Add a todo</button>
+    </div>
+}
