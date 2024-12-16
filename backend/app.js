87431bb33bcc907a6a65e0989c2c2c85e62c4ce5
raashed <mohammedraashed10@gmail.com>
2024-12-16 14:11:26 +0530

    adding HTTP methods

--- backend/app.js
+++ backend/app.js
@@ -5,10 +5,20 @@ const port = 3000;
 app.use(express.json());
 
 // creating all the todos
+app.post("/todo", (req, res) => {
+    // adding the logic for input validation
+
+});
 
 // seeing all the todos
+app.get("/todos", (req, res) => {
+
+});
 
 // marking todos as done aka updating all the todos
+app.put("/completed", (req, res) => {
+    // adding the logic for input validation
+})
 
 app.listen(port, () => {
     console.log(`you server is listening on the port ${port}`);
