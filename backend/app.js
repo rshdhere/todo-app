94d17981dba4a94ace85d35b108c59ffe4b20bc7
raashed <mohammedraashed10@gmail.com>
2024-12-16 14:21:55 +0530

    added input validation to the endpoints

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { createTodo, updateTodo } = require("./types");
 const app = express();
 const port = 3000;
 
@@ -7,6 +8,15 @@ app.use(express.json());
 // creating all the todos
 app.post("/todo", (req, res) => {
     // adding the logic for input validation
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if (!parsedPayload.success){
+        res.status(411).json({
+            msg : "you entered the wrong inputs"
+        });
+        return;
+    }
+    // add mongodb here
 
 });
 
@@ -18,6 +28,14 @@ app.get("/todos", (req, res) => {
 // marking todos as done aka updating all the todos
 app.put("/completed", (req, res) => {
     // adding the logic for input validation
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload);
+    if (!parsedPayload.success){
+        res.status(411).json({
+            msg : "You sent the wrong inputs"
+        })
+        return;
+    }
 })
 
 app.listen(port, () => {
