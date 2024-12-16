797a91507a08e42ae97708fb3a744f5335ce3003
raashed <mohammedraashed10@gmail.com>
2024-12-16 14:30:59 +0530

    database was connected

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { createTodo, updateTodo } = require("./types");
+const { connectDB } = require("./database");
 const app = express();
 const port = 3000;
 
