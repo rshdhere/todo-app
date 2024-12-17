8f85541a3c1201542fa79fdc5b442783b2f72462
raashed <mohammedraashed10@gmail.com>
2024-12-17 19:45:09 +0530

    installing CORS

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -9,6 +9,9 @@ export function CreateTodo(){
             margin : 10
         }} type="text" placeholder="description"/> <br></br>
 
-        <button> Add a todo</button>
+        <button style = {{
+            padding : 10,
+            margin : 10
+        }}> Add a todo</button>
     </div>
 }
