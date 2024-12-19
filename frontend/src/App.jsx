f3cf5264978197f3b6e0610b7884b7450ccf40b1
raashed <mohammedraashed10@gmail.com>
2024-12-19 09:21:23 +0530

    complete almost (infinite requests)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,12 @@ import { Todos } from "./components/Todos"
 
 function App() {
 const [todos, setTodos] = useState([]);
+
+fetch("http://localhost:3000/todos")
+.then(async function(res){
+  const json = await res.json();
+  setTodos(json.allTodos);
+})
   return (
     <>
       <div>
--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,22 +1,50 @@
+import { useState } from "react";
+
 export function CreateTodo(){
+    
+    // react-query
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    
     return <div>
         
-        <input style={{
+        <input id="title" style={{
             padding : 10,
             margin : 20,
             marginBottom : 0
-        }} type="text" placeholder="title"/> <br/>
+        }} type="text" placeholder="title" onChange={function(e){
+            const value = e.target.value;
+            setTitle(value)
+        }}/> <br/>
         
-        <input style={{
+        <input id="desc" style={{
             padding : 10,
             margin : 20,
             marginTop : 10,
-        }} type="text" placeholder="description"/> <br/>
+        }} type="text" placeholder="description" onChange={function(e){
+            const value = e.target.value;
+            setDescription(value)
+        }}/> <br/>
 
         <button style={{
             padding : 10,
             marginLeft : 75,
            
+        }} onClick={() => {
+            fetch("http://localhost:3000/todo", {
+                method : "POST",
+                body : JSON.stringify({
+                    title : title,
+                    description : description
+                }), 
+                headers : {
+                    "Content-type" : "application/json"
+                }
+            })
+            .then(async function (res) {
+                const json = await res.json();
+                alert("Todo Added");
+            })
         }}>Add a todo</button>
     </div>
 }
